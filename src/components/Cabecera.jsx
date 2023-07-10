c7e48790dab55362adca314f2a2b856918d5c2df
contrerasagudo <contrerasagudo@gmail.com>
2023-07-10 00:03:44 -0300

    Efectos hover sobre las secciones , cambio de color de border-bottom

--- src/components/Cabecera.jsx
+++ src/components/Cabecera.jsx
@@ -10,22 +10,22 @@ import './cabecera.css'
 
 function NavScrollExample() {
     return (
-        <Navbar expand="lg" className="bg-body-tertiary" data-bs-theme="light" bg="light" 
-         >
-            <Container fluid >
+        
+        <Navbar expand="lg" className="bg-body-tertiary" data-bs-theme="light" bg="light" >
+            <Container fluid>
                 <Navbar.Brand href="#"><img src={logo2} alt="" /></Navbar.Brand>
                 <Navbar.Toggle aria-controls="navbarScroll" />
                 <Navbar.Collapse id="navbarScroll">
                     <Nav
-                        className="me-0 my-2 my-lg-0"
+                        className="meca me-0 my-2 my-lg-0"
                         style={{ maxHeight: "200px" }}
                         navbarScroll
                     >
                         <Nav.Link  href="#action1">Conocenos</Nav.Link>
-                        <Nav.Link href="#action2">Servicios</Nav.Link>
-                        <Nav.Link href="#action3">Productos</Nav.Link>
-                        <Nav.Link href="#action4">Contactanos</Nav.Link>
-                        <Nav.Link href="#action4">Login</Nav.Link>
+                        <Nav.Link  href="#action2">Servicios</Nav.Link>
+                        <Nav.Link  href="#action3">Productos</Nav.Link>
+                        <Nav.Link  href="#action4">Contactanos</Nav.Link>
+                        <Nav.Link  href="#action4">Login</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
