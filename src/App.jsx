6cf836782134d5936634b899b132a301ba412e82
contrerasagudo <contrerasagudo@gmail.com>
2023-07-22 22:23:39 -0300

    Añadida seccion de planes

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,26 @@
-import './App.css'
-import Header from './components/PaginaPrincipal/Header/Header'
-import Slider from './components/PaginaPrincipal/Slider/Slider'
-import Footer from './components/PaginaPrincipal/Footer/Footer'
-import Servicios from './components/PaginaPrincipal/Servicios/Servicos'
-
-
-
+import "./App.css";
+import Header from "./components/PaginaPrincipal/Header/Header";
+import Slider from "./components/PaginaPrincipal/Slider/Slider";
+import Footer from "./components/PaginaPrincipal/Footer/Footer";
+import Servicios from "./components/PaginaPrincipal/Servicios/Servicos";
+import Planes from "./components/PaginaPrincipal/Planes/Planes";
 
 function App() {
   return (
     <>
-    <div className="principals">
-      <Header/>
-    </div>
-    <div className='slider'>
-      <Slider/>
-    </div>
-    <Servicios/>
-    <div>
-      <Footer/>
-    </div>
-
-
+      <div className="principals">
+        <Header />
+      </div>
+      <div className="slider">
+        <Slider />
+      </div>
+      <Servicios />
+      <Planes />
+      <div>
+        <Footer />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
