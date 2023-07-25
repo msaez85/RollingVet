e7ec88429338c3d88b2ce6721dfce61e8ad59266
d9ran <duranluciano123@gmail.com>
2023-07-25 11:36:17 -0300

    contacto + acercanosotros

---
+++ src/components/contacto/contacto.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import './contacto.css';
+
+const Contacto = () => {
+  return (
+  
+    <div className="container">
+      <div className="row">
+      
+        
+        <div className="col-md-6">
+        <h1 className="mb-5">Contáctanos</h1>
+        <div className="contact-form">
+          <div className="form-group">
+            <input type="text" className="form-control" placeholder="Nombre" />
+          </div>
+          <div className="form-group">
+            <div className="input-group">
+              <div className="input-group-prepend">
+                
+              </div>
+              <select className="form-control">
+                <option value="USA">🇺🇸 +1</option>
+                <option value="ARG">🇦🇷 +54</option>
+                <option value="MEX">🇲🇽 +52</option>
+                <option value="BRA">🇧🇷 +55</option>
+              </select>
+              <input type="text" className="form-control" placeholder="Número telefónico" />
+            </div>
+          </div>
+          <div className="form-group">
+            <input type="email" className="form-control" placeholder="Email" />
+          </div>
+          <div className="form-group">
+            <textarea className="form-control" placeholder="Comentario o consulta" />
+          </div>
+          <button className="btn btn-primary btn-block">Enviar</button>
+        </div>
+      
+        
+      
+        </div>
+      
+        <div className="col-md-6 d-flex align-items-center justify-content-center">
+            <img
+              src="https://hips.hearstapps.com/hmg-prod/images/perros-vivir-mas-anos-1669733501.jpg"
+              alt="Perro"
+              className="img-fluid"
+            />
+      </div>
+      </div>
+    </div>  
+    
+  );
+};
+
+export default Contacto;
